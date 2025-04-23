009252291517b82937b9f6c14ed6d7491cbcb820
mtadim <mtadim@techsocial.com.br>
2025-04-23 20:04:26 -0300

    [curso React] Aula 4 - Atividade 2 Criando o componente Time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
+import { useState } from "react";
+import Time from './componentes/Time';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([]);
+
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />     
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/> 
+      <Time nome="Programação"/>
+      <Time nome="Frontend"/>
     </div>
   );
 }
