966417b6ad2dea123a54c984c424153d1ca03956
mtadim <mtadim@techsocial.com.br>
2025-04-23 21:05:22 -0300

    [curso React] Aula 4 - Atividade 4 Refatorando código duplicado

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,16 +6,6 @@ import "./Formulario.css";
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
-
     //SETA VALORES  DO FORMULARIO
     const [nome, setNome] = useState('');
     const [cargo, setCargo] = useState('');
@@ -58,7 +48,7 @@ const Formulario = (props) => {
                 <ListaSuspensa 
                     obrigatorio={true}
                     label="Time"
-                    itens={times}
+                    itens={props.nomeDosTimes}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                     />
