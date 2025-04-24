03c7084bf1d4a0f11e217d6ebb44bf6edaacde7f
mtadim <mtadim@techsocial.com.br>
2025-04-23 22:20:55 -0300

    [curso React] Aula 4 - Atividade 7 Escondendo times vazios

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -6,7 +6,7 @@ const Time = (props) => {
     const colorSecundaria = { backgroundColor: props.corSecundaria }
 
     return(
-        <section className='time' style={colorSecundaria}>
+        (props.colaboradores.length > 0) ? <section className='time' style={colorSecundaria}>
             <h3 style={{ borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
             { props.colaboradores.map( colaborador => <Colaborador                             
@@ -17,6 +17,7 @@ const Time = (props) => {
             )}
             </div>
         </section>
+        : ''
     )
 }
 export default Time
