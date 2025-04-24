d53744c461a9f837076b129f0e7c053d998f4c0d
mtadim <mtadim@techsocial.com.br>
2025-04-23 21:23:30 -0300

    [curso React] Aula 4 - Atividade 5 Card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/celostad.png' alt='Marcelo Tadim'/>
+            </div>
+            <div className='rodape'>
+                <h4>Marcelo Tadim</h4>
+                <h5>Desenvolvedor PHP Senior</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,3 +1,4 @@
+import Colaborador from '../Colaborador';
 import './Time.css';
 
 const Time = (props) => {
@@ -7,6 +8,8 @@ const Time = (props) => {
     return(
         <section className='time' style={colorSecundaria}>
             <h3 style={{ borderColor: props.corPrimaria}}>{props.nome}</h3>
+            <Colaborador />
+            <Colaborador />
         </section>
     )
 }
